2580d16029d7ee16f81cd47b36910975235de569
imkarin <karinmeijvogel@hotmail.com>
2021-11-22 15:20:10 +0100

    Seeing lifecycle hooks in action

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,20 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentChecked, 
+         AfterContentInit, 
+         AfterViewChecked, 
+         AfterViewInit, 
+         Component, 
+         DoCheck, 
+         Input, 
+         OnChanges, 
+         OnDestroy, 
+         OnInit, 
+         SimpleChanges 
+        } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  // add the @Input decorator, so this component can receive 'elements' from the parent component
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, 
+  AfterViewInit, AfterViewChecked, OnDestroy { // Good practice to implement the lifecycle hooks
   @Input() element: {    
     type: string, 
     name: string, 
-    content: string };  // typescript: defining what the element type looks like
+    content: string };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called!')
   }
 
+  ngOnChanges(changes: SimpleChanges) { // ngOnChanges is called before ngOnInit, this is called because of the @Input 'element' property changing
+    console.log('ngOnChanges called!')
+    console.log(changes)
+  }
+  
+  ngOnInit(): void {
+    console.log('ngOnInit called!')
+  }
+
+  ngDoCheck() { // run by angular to check for changes, triggered by all kinds of things, like: event being called, promise giving back data, ...
+    console.log('ngDoCheck called!')
+  }
+
+  ngAfterContentInit() { // runs when content gets projected into view through ng-content directive
+    console.log('ngAfterContentInit called!')
+  }
+  
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!')
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!')
+  }
+
+  ngOnDestroy() { // Called after component get destroyed, for example in the parent app-component, you remove a server from the list
+    console.log('ngOnDestroy called!')
+  }
 }
