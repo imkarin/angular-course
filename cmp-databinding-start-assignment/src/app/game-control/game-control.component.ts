e37752711e94c07d40797cd62a69c16cee0e5e96
imkarin <karinmeijvogel@hotmail.com>
2021-11-24 10:41:08 +0100

    Added gamecontrol logic: startGame and stopGame outputs currentNumber

--- cmp-databinding-start-assignment/src/app/game-control/game-control.component.ts
+++ cmp-databinding-start-assignment/src/app/game-control/game-control.component.ts
@@ -7,18 +7,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
   @Output() numberUpdated = new EventEmitter<number>();
+  currentNumber: number = 0;
+  interval: ReturnType<typeof setInterval> = setInterval(() => {}, 5000); // random default value so ts doesn't complain
+  gameRunning: boolean = false; // disables Start/Stop button
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   startGame() {
+    this.gameRunning = true;
+    this.interval = setInterval(() => {
+      this.currentNumber += 1;
+      this.numberUpdated.emit(this.currentNumber);
+    }, 1000)
   }
-
+  
   stopGame() {
+    this.gameRunning = false;
+    this.currentNumber = 0;
+    clearInterval(this.interval); // stop the interval
+
+    // outputs 0, will be trigger for deleting all current numbers from the page:
+    this.numberUpdated.emit(this.currentNumber); 
   }
-
-  // function to emit numberUpdated (every second: number+=1)
-
 }
