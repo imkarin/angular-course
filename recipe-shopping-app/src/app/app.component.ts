5c2d090ace9bad8745d0a27b2b64ca62d1e4f9be
imkarin <karinmeijvogel@hotmail.com>
2021-11-25 11:25:30 +0100

    Added navigation by emitting 'selectedFeature' from the header/nav component

--- recipe-shopping-app/src/app/app.component.ts
+++ recipe-shopping-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-shopping-app';
+  selectedFeature = 'recipe'; // default value, spelled the same as in header component
+
+  onNavigate(feature: string) {
+    this.selectedFeature = feature;
+    console.log(this.selectedFeature)
+  }
 }
