f91995d31f4b9d4418dd82be2059252805d80671
imkarin <karinmeijvogel@hotmail.com>
2021-11-25 12:26:03 +0100

    Practicing localreferences, shopping-edit form works (passes added ingr to shopping-list)

--- recipe-shopping-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addIngredient(ingredientName: string, ingredientAmount: number) {
+    this.ingredientAdded.emit(new Ingredient(ingredientName, ingredientAmount));
   }
 
 }
