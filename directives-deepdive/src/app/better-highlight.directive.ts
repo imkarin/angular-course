f2c1bdffa88993531bce08a2e636a2576961efe0
imkarin <karinmeijvogel@hotmail.com>
2021-11-30 13:39:24 +0100

    Binding to properties of the directive with @Input()

--- directives-deepdive/src/app/better-highlight.directive.ts
+++ directives-deepdive/src/app/better-highlight.directive.ts
@@ -1,27 +1,31 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') bgColor: string = 'red'; // accessing the DOM property style.backgroundColor
+  @Input() highlightColor: string = 'blue';
+  @Input() defaultColor: string = 'red';
+
+  @HostBinding('style.backgroundColor') bgColor: string = ''; // accessing the DOM property style.backgroundColor
   // bind to any property of the element you're sitting on!
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { } 
   
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red')
     // elRef is still the ref to the element that this directive is on
     // last argument of setStyle: flags. For example '!important' in the css. 
+    this.bgColor = this.defaultColor;
   }
   
   @HostListener('mouseenter') mouseover() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.bgColor = 'blue';
+    this.bgColor = this.highlightColor;
   }
   
   @HostListener('mouseleave') mouseleave() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
-    this.bgColor = 'red';
+    this.bgColor = this.defaultColor;
   }
 }
