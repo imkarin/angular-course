2da320607ea46559141fd3f513ff3a63f07646b3
imkarin <karinmeijvogel@hotmail.com>
2021-12-01 10:09:48 +0100

    Structural directives behind the scenes: *ngIf (ng-template [ngIf])

--- directives-deepdive/src/app/app.component.ts
+++ directives-deepdive/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  greeting: string = 'Hello';
+
+  toggleGreeting() {
+    this.greeting = this.greeting === 'Hello' ? 'Bye' : 'Hello';
+  }
 }
