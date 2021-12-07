c67cbad58fdc6b527adedd0968a7560f92421f41
imkarin <karinmeijvogel@hotmail.com>
2021-12-07 14:21:48 +0100

    Subscribing to route.params to reactively fetch route params in loaded component

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -12,10 +12,21 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.user = {
-      id: this.route.snapshot.params['id'], // get the route param 'id'
-      name: this.route.snapshot.params['name'], // get the route param 'name'
-    }
-  }
+    // this.user = {
+    //   id: this.route.snapshot.params['id'], // get the route param 'id'
+    //   name: this.route.snapshot.params['name'], // get the route param 'name'
+    // }
 
+    // ^ Above code won't change this.user if the component is loaded again
+    // with different params: it will just show the old user id/name
+    // Therefore we subscribe to the params observable instead:
+    this.route.params.subscribe(
+      (updatedParams) => {
+        this.user = {
+          id: updatedParams['id'],
+          name: updatedParams['name']
+        }
+      }
+    )
+  }
 }
