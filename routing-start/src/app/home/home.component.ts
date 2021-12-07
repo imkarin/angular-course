9f847a90879ad417d0ce97a9751f0cd5fee20d6f
imkarin <karinmeijvogel@hotmail.com>
2021-12-07 13:30:48 +0100

    Added comment about router.navigate

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  // function that gets called by some button (without a link)
+  onLoadServers() {
+    // complex calculation
+    // we need to get access to our router and tell it: "go here"
+    // we do this by injecting it into the constructor
+    this.router.navigate(['/servers'])
+  }
 }
