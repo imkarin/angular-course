6e6215b56ad106de71e957a8897ceb7950d0c4e9
imkarin <karinmeijvogel@hotmail.com>
2021-12-10 13:56:48 +0100

    Extra comments auth.service about promises/resolve

--- routing-start/src/app/auth.service.ts
+++ routing-start/src/app/auth.service.ts
@@ -5,8 +5,8 @@ export class AuthService {
 
     isAuthenticated() {
         const promise = new Promise((resolveFunc, rejectFunc) => {
-            setTimeout(() => {
-                resolveFunc(this.loggedIn); // fake that the login takes 800 ms
+            setTimeout(() => { // fake that the login takes 800 ms
+                resolveFunc(this.loggedIn); // this will make the promise^ resolve with the value of this.loggedIn
             }, 800);
         });
         return promise;
