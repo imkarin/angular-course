70e38f41c2cd0d03fdc3cfa415094e11478b0e86
imkarin <karinmeijvogel@hotmail.com>
2022-01-13 16:11:03 +0100

    Added /edit and /new link for recipes

--- recipe-shopping-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-shopping-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,8 @@
+import { areAllEquivalent } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.scss']
 })
 export class RecipeEditComponent implements OnInit {
+  recipeId: number;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // determine: are we in /new or in /:id/edit?
+    this.route.params.subscribe((routeParams: Params) => {
+      this.recipeId = +routeParams['id'];
+      this.editMode = routeParams['id'] != null;
+      console.log(this.recipeId)
+    })
   }
 
 }
