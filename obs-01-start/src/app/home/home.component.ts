89b67983f845c18c61942a2fd47c411d146e07df
imkarin <karinmeijvogel@hotmail.com>
2022-01-13 20:07:11 +0100

    Making first own observable (observer.next)

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,24 +9,33 @@ import { interval, Subscription } from 'rxjs';
 export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription: Subscription;
-
+  private customObsSubscription: Subscription;
+  
   constructor() { }
 
   ngOnInit() {
-  //   interval(1000).subscribe(count => { // every 1 second, a value gets emitted which we subscribe to
-  //     console.log(count) // 'count' is the value that came from interval() function
-  //   }) // this observable will keep running even when you go to a different page/component, so you should unsubscribe from them
-  // to prevent memory leaks (btw: every time you click this component, a new observable will start)
-  
-  // you can prevent new observables being started every time, by storing it in a variable:
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+  // // you can prevent new observables being started every time, by storing it in a variable:
+  // // and later unsubscribing from this variable/subscription in ngOnDestroy()
+  //   this.firstObsSubscription = interval(1000).subscribe(count => {
+  //     console.log(count);
+  //   })
+
+  // ^ Building this observable manually
+    const customIntervalObservable = new Observable(observer => { // observer = the part that is interested in being informed about new data etc.
+      let count = 0;
+      setInterval(() => { // using the normal js setInterval function
+        observer.next(count);  // .next emits a new value
+        count++;
+      }, 1000);
     })
 
-  // and later unsubscribing from this variable/subscription in ngOnDestroy()
+    this.customObsSubscription = customIntervalObservable.subscribe(count => {
+      console.log(count);
+    })
   }
 
   ngOnDestroy() {
-    this.firstObsSubscription.unsubscribe();
+    // this.firstObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 }
