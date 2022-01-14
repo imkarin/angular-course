707a4b4c7f32d40bb0bc3b6ea1999da4f15e61b7
imkarin <karinmeijvogel@hotmail.com>
2022-01-14 14:55:32 +0100

    Using Subjects for cross-component communication

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(private userService: UserService) {}
+  
+  buttonActivatedSubscription
 
-  ngOnInit() {}
+  activated = false;
+
+  ngOnInit() {
+    this.buttonActivatedSubscription = this.userService.buttonActivateSubject.subscribe((buttonActive => {
+      this.activated = buttonActive;
+    }))
+  }
+
+  ngOnDestroy() {
+    this.buttonActivatedSubscription.unsubcribe();
+  }
 }
