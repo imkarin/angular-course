14993e8dd105b8659f5a04a13995278e78a17534
imkarin <karinmeijvogel@hotmail.com>
2022-01-17 14:09:54 +0100

    Using two-way databinding with ngModel

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,7 +9,8 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   // Getting the form data with viewchild allows you to get the data anytime you want
   // since you can now use this myForm variable whenever
-  @ViewChild('f') myForm;
+  @ViewChild('f') myForm: NgForm;
+  secretAnswer: string = "Answer";
 
   suggestUserName() {
     const suggestedName = 'Superuser';
