8706cf8c77739338760e9bb9cbbd0588605d5417
imkarin <karinmeijvogel@hotmail.com>
2022-01-17 20:06:57 +0100

    Reactive: FormArray filled with FormControls

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,12 +17,24 @@ export class AppComponent {
         'username': new FormControl('SuperUser', Validators.required), 
         'email': new FormControl(null, [Validators.required, Validators.email]), // you can also add an array of validators
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([]) // array of form-controls
     });
   }
 
   onSubmit() { // we don't need a localreference to the form in html, 
                // we already have it as a variable in this code
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null); // the user will set this value
+    // you need to tell TS here that this is of type FormArray (cast it) so you don't get an error
+    // this kind of situation is rare:
+    (<FormArray>this.signupForm.get('hobbies')).push(control); // you push a new control to the array in the signupForm
+    // pressing the button creates a new FormControl (which we linked in the html to a text-input field)
+    // it will be empty initially, but the user can type in it.
+    // the user can add as many form controls as they want.
+    // ng docs: FormArray tracks the value and validity state of an array of FormControl, FormGroup or FormArray instances.
   }
 }
