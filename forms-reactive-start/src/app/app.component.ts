4ddc144e44bdc4982c34c356b5dfee22d4c2072e
imkarin <karinmeijvogel@hotmail.com>
2022-01-17 20:32:33 +0100

    Reactive: Making custom validator and binding this-keyword

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,12 +9,14 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   genders = ['male', 'female'];
   signupForm: FormGroup; // in angular, a form = just a group of controls
+  forbiddenUsernames: string[] = ['Admin', 'Moderator'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       // you add controls here in key-value pairs
       'userData': new FormGroup({
-        'username': new FormControl('SuperUser', Validators.required), 
+        'username': new FormControl('SuperUser', [Validators.required, this.forbiddenNamesCheck.bind(this)]), 
+        // bind: good old javascript trick to make sure that "this" refers to this component
         'email': new FormControl(null, [Validators.required, Validators.email]), // you can also add an array of validators
       }),
       'gender': new FormControl('male'),
@@ -36,5 +38,19 @@ export class AppComponent {
     // it will be empty initially, but the user can type in it.
     // the user can add as many form controls as they want.
     // ng docs: FormArray tracks the value and validity state of an array of FormControl, FormGroup or FormArray instances.
+  }
+
+  // Making our own validator
+  // A validator is just a function
+  forbiddenNamesCheck(control: FormControl): {[s: string]: boolean} {
+    // check if the value of the control is part of the forbiddenUserNames array
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+    // Gotcha: 'this'^ word will not automatically be recognizd as this component
+    // angular is the one calling the code somewhere, so 'this' doesn't refer to this component anymore
+    // you have to BIND this (up in the signupForm) to our 'this'
+      return {'usernameIsForbidden': true};
+    }
+    return null; // IMPORTANT: if validation is successfull, you have to pass nothing or null
+    // that's how you tell angular that a form control is valid
   }
 }
