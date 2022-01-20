308f1841fa3a12b7f723f4f4373fa06c18a9e683
imkarin <karinmeijvogel@hotmail.com>
2022-01-20 15:22:45 +0100

    Providing shopping-list service in app.module so shopping-list remains when navigating away

--- recipe-shopping-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-list.component.ts
@@ -5,8 +5,7 @@ import { ShoppingListService } from './shopping-list.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss'],
-  providers: [ShoppingListService]
+  styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
