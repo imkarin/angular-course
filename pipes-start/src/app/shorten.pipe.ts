a360759477d9ca1d568fd9e7c5f60ad1e6b69932
imkarin <karinmeijvogel@hotmail.com>
2022-01-24 19:34:46 +0100

    Making custom 'shorten' pipe

---
+++ pipes-start/src/app/shorten.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+    // It's not always nessecary to implement PipeTransform interface, but it's good practice
+    transform(value: any) { // 2nd/3rd... arguments would be arguments that you pass to your pipe (like date:format)
+        let newVal = value.substr(0, 5) 
+        if (value.length > 5) {
+             newVal = newVal += '...';
+        }
+        return newVal // always needs to return something
+    }
+}
+
+// Don't forget to add this pipe to your module
