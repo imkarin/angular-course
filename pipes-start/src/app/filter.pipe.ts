7c617560fe29b90b3344fa50af321e79169b211d
imkarin <karinmeijvogel@hotmail.com>
2022-01-24 19:48:35 +0100

    Filtering ngFor loop output with custom pipe

---
+++ pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: string, propName: string): any {
+    if (value.length === 0 || filterString === undefined || filterString === '') {
+      return value;
+    }
+
+    const filteredArray = [];
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        filteredArray.push(item);
+      }
+    }
+    return filteredArray;
+
+  }
+}
+
+// automatically gets the decorator and gets added to module 
+// if you generate pipe with the CLI
