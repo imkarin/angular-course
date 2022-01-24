6056c890d2bf482dd59bcdc9987514b3469589b5
imkarin <karinmeijvogel@hotmail.com>
2022-01-24 19:36:05 +0100

    Adding parameters to custom pipe

--- pipes-start/src/app/shorten.pipe.ts
+++ pipes-start/src/app/shorten.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class ShortenPipe implements PipeTransform {
     // It's not always nessecary to implement PipeTransform interface, but it's good practice
-    transform(value: any) { // 2nd/3rd... arguments would be arguments that you pass to your pipe (like date:format)
-        let newVal = value.substr(0, 5) 
-        if (value.length > 5) {
+    transform(value: any, length: number) { // 2nd/3rd... arguments would be arguments that you pass to your pipe (like date:format)
+        let newVal = value.substr(0, length) 
+        if (value.length > length) {
              newVal = newVal += '...';
         }
         return newVal // always needs to return something
