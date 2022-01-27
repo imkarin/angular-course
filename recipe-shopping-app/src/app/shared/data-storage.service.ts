afeda1f74a4a64ad31d746d45e4741889a207de6
imkarin <karinmeijvogel@hotmail.com>
2022-01-27 15:10:03 +0100

    Added (hidden) api baseurl through a custom local environment

--- recipe-shopping-app/src/app/shared/data-storage.service.ts
+++ recipe-shopping-app/src/app/shared/data-storage.service.ts
@@ -1,19 +1,22 @@
 // Service for Http functionality, storing data
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 // You NEED Injectable decorator, because you're going to inject the http service into it.
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
+    private fireBaseUrl = environment["FIREBASE_URL"]
+
     constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
 
         this.http.put<Recipe[]>(
-            'url_redacted',
+            this.fireBaseUrl + '/recipes.json',
             recipes
         )
         .subscribe(res => {
