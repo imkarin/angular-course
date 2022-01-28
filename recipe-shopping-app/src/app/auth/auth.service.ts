4e8c831a7dc8a217a7c038678ee5a9c5224e7fc9
imkarin <karinmeijvogel@hotmail.com>
2022-01-28 15:13:13 +0100

    Passing AuthResponseData interface to generic http.post<> method

--- recipe-shopping-app/src/app/auth/auth.service.ts
+++ recipe-shopping-app/src/app/auth/auth.service.ts
@@ -3,18 +3,26 @@ import { ReturnStatement } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+interface AuthResponseData {
+  kind: string,
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private fireBaseUrl = environment["FIREBASE_URL"]
   private fireBaseApiKey = environment["FIREBASE_APIKEY"]
 
   constructor(private http: HttpClient) { }
 
   signUp(email: string, password: string) {
     console.log(email, password)
-    this.http.post(
+    this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.fireBaseApiKey,
       {
         email: email,
