62916171584f79f804a32bcba1f3943eb20129b8
imkarin <karinmeijvogel@hotmail.com>
2022-01-28 14:50:48 +0100

    Making auth form functional

--- recipe-shopping-app/src/app/auth/auth/auth.component.ts
+++ recipe-shopping-app/src/app/auth/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -18,5 +19,10 @@ export class AuthComponent implements OnInit {
     this.loginMode = !this.loginMode;
     console.log(this.loginMode);
   }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  } 
 
 }
