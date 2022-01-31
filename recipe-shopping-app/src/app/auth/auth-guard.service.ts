eed39508e2723f71eae8a2b017e5ba98bacd5748
imkarin <karinmeijvogel@hotmail.com>
2022-01-31 17:46:47 +0100

    Improve auth guard by only listening to currentUser obs once

--- recipe-shopping-app/src/app/auth/auth-guard.service.ts
+++ recipe-shopping-app/src/app/auth/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, take } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
 @Injectable({providedIn: 'root'})
@@ -12,13 +12,16 @@ export class AuthGuard implements CanActivate {
         route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot
         ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.authService.currentUser.pipe(map(currentUser => { // remember our user is an observable
-            const isAuth = !!currentUser; // converts a true-ish value (anything thats not null or undefined) to true, and false-ish to false
-            if (isAuth) {
-                return true;
-            } else {
-                return this.router.createUrlTree(['/auth']);
-            }
-        }));
+        return this.authService.currentUser.pipe(
+            take(1), // only listen for currentUser once, if the guard kept listening, strange behaviour might occur
+            map(currentUser => { // remember our user is an observable
+                const isAuth = !!currentUser; // converts a true-ish value (anything thats not null or undefined) to true, and false-ish to false
+                if (isAuth) {
+                    return true;
+                } else {
+                    return this.router.createUrlTree(['/auth']);
+                }
+            })
+        );
     }
 }
