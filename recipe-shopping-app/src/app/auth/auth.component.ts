02fc1943c0673be0de38e7093b6095ebde9732dc
imkarin <karinmeijvogel@hotmail.com>
2022-01-31 20:11:04 +0100

    Data and event binding to dynamic component instance

--- recipe-shopping-app/src/app/auth/auth.component.ts
+++ recipe-shopping-app/src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import { AuthResponseData, AuthService } from './auth.service';
@@ -11,12 +11,13 @@ import { AuthResponseData, AuthService } from './auth.service';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
 
   loginMode = true;
   loading = false;
   // error: string = null; // you don't need this global error with this approach
   @ViewChild(PlaceholderDirective) alertHost; // you can pass a type to viewchild, it will look for the first place where this type (this directive) is used
+  private closeAlertSub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -76,9 +77,21 @@ export class AuthComponent implements OnInit {
     // you can use the factory to create a complete component
     // we will place the component in the ng-template with the appPlaceholder directive
     const hostViewContainerRef = this.alertHost.viewContainerRef;
-    // hostViewContainerRef.clear(); // make sure there's no components in it already
-    hostViewContainerRef.createComponent(alertCmpFactory);
+    hostViewContainerRef.clear(); // make sure there's no components in it already
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
 
+    // access the concrete instance of the component we created
+    componentRef.instance.message = message;
+    this.closeAlertSub = componentRef.instance.closeAlert.subscribe(() => { // this is the only exception, where subscribing to an EventEmitter is OK
+      this.closeAlertSub.unsubscribe(); // cleans itself up immediately after being clicked/closed
+      hostViewContainerRef.clear();
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeAlertSub) { // clean up subscription in case we leave the component without clicking 'close'
+      this.closeAlertSub.unsubscribe();
+    }
   }
 
 }
