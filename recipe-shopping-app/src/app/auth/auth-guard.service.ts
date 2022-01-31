f17c6309f2508a64e6f212c6d779274b7267dd1c
imkarin <karinmeijvogel@hotmail.com>
2022-01-31 17:45:07 +0100

    Redirect user to auth page if not authenticated

--- recipe-shopping-app/src/app/auth/auth-guard.service.ts
+++ recipe-shopping-app/src/app/auth/auth-guard.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-    constructor (private authService: AuthService) {}
+    constructor (private authService: AuthService, private router: Router) {}
 
     canActivate(
         route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot
         ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         return this.authService.currentUser.pipe(map(currentUser => { // remember our user is an observable
-            return !!currentUser; // converts a true-ish value (anything thats not null or undefined) to true, and false-ish to false
+            const isAuth = !!currentUser; // converts a true-ish value (anything thats not null or undefined) to true, and false-ish to false
+            if (isAuth) {
+                return true;
+            } else {
+                return this.router.createUrlTree(['/auth']);
+            }
         }));
     }
 }
