de5339c2c4c9285a8569b34b6a72ed890f792398
imkarin <karinmeijvogel@hotmail.com>
2022-02-01 13:49:36 +0100

    Create recipe feature module (splitting modules wrongly)

--- recipe-shopping-app/src/app/app.module.ts
+++ recipe-shopping-app/src/app/app.module.ts
@@ -5,36 +5,28 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
+
+import { DropdownDirective } from './shared/dropdown.directive';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
+
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
+import { RecipesModule } from './recipes/recipes.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -44,7 +36,8 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipesModule
   ],
   providers: [
     ShoppingListService, 
---
+++ recipe-shopping-app/src/app/recipes/recipes.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes.component";
+
+// This module should be responsible for defining the building blocks for the recipe feature area
+
+@NgModule({
+    declarations: [ // all recipe-related components should be here
+        RecipesComponent, // remove them from the AppModule
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+    ],
+    exports: [ // export all components, so that they can be used in any module that imports this module
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+    ]
+})
+export class RecipesModule { }
