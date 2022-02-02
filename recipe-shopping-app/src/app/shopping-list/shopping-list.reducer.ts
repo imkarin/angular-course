f9baa7684e8b49e5b709321b985f4e733c42b032
imkarin <karinmeijvogel@hotmail.com>
2022-02-02 15:33:14 +0100

    Update the shoppingListReducer

--- recipe-shopping-app/src/app/shopping-list/shopping-list.reducer.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,5 +1,6 @@
 // We'll use NgRx to replace our service-rxjs approach to
 // managing the Application State
+import { Action } from "@ngrx/store";
 import { Ingredient } from "../shared/ingredient.model";
 
 // NgRx reducer is just a function that takes in the state and a
@@ -10,7 +11,7 @@ import { Ingredient } from "../shared/ingredient.model";
 // the NgRx package will eventually call this function and pass
 // these arguments into them
 
-// Uou can set an initial state. your state should be a js object
+// You can set an initial state. your state should be a js object
 // most of the time (that way you can group multiple, diff kinds
 // of data together in the state)
 const initialState = {
@@ -19,6 +20,21 @@ const initialState = {
         new Ingredient('Bananas', 5)
     ]
 }
-export function shoppingListReducer(state, action) {
 
+// Pass initialState as default state
+export function shoppingListReducer(state = initialState, action: Action) {
+    // Find out which kind of action was dispatched
+    switch(action.type) {
+        // Which action types your app has, is totally up to you
+        case 'ADD_INGREDIENT': // convention = all caps type names
+            // NEVER touch the previous state: it's forbidden, it should be immmutable
+            // Reducer always returns a (new/modified copy of the) state
+            return {
+                ...state,
+                ingredients: [
+                    ... state.ingredients,
+                    action // this is wrong, we'll look at actions later
+                ]
+            }
+    }
 }
