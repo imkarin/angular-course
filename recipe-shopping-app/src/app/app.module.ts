a7ffc77ed483ff20ea2ad7d748721025408b1781
imkarin <karinmeijvogel@hotmail.com>
2022-02-03 17:04:03 +0100

    Remove unused ShoppingListService

--- recipe-shopping-app/src/app/app.module.ts
+++ recipe-shopping-app/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,9 +29,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
     AppRoutingModule,
     SharedModule,
   ],
-  providers: [
-    ShoppingListService, 
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
